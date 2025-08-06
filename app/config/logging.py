96b27320719c3675eb0ab438a33dccd6c7eeefe4
RocketMenace <grizzlygit777@gmail.com>
2025-08-06 23:46:34 +0300

    build: Setup logger.

---
+++ app/config/logging.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s")
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setFormatter(fmt=formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+logger = setup_logging()
